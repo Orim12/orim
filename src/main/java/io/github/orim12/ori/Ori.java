4d8f9bb77841f637d2c61b01ccffc2f7a51e06ca
Orim12 <miro.vaassen@gmail.com>
2024-07-08 16:16:50 +0200

    e

--- src/main/java/io/github/orim12/ori/Ori.java
+++ src/main/java/io/github/orim12/ori/Ori.java
@@ -1,9 +1,12 @@
 package io.github.orim12.ori;
 
+import io.github.orim12.ori.item.ModItems;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.rmi.registry.Registry;
 
 
 public class Ori implements ModInitializer {
@@ -12,6 +15,7 @@ public class Ori implements ModInitializer {
 
 	@Override
 	public void onInitialize(ModContainer mod) {
-
+		LOGGER.info("Initializing Ori");
+		ModItems.registerItems();
 	}
 }
--- src/main/java/io/github/orim12/ori/item/ModItems.java
+++ src/main/java/io/github/orim12/ori/item/ModItems.java
@@ -1,4 +1,27 @@
 package io.github.orim12.ori.item;
 
+import io.github.orim12.ori.Ori;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
+
 public class ModItems {
+
+	// items
+	public static final Item RAW_TANZANITE = registerItem("raw_tanzanite",
+		new Item(new QuiltItemSettings().group(ItemGroup.MISC)));
+	public static final Item TANZANITE = registerItem("tanzanite",
+		new Item(new QuiltItemSettings().group(ItemGroup.MISC)));
+
+
+	private static Item registerItem(String name, Item item) {
+		return Registry.register(Registry.ITEM, new Identifier(Ori.MOD_ID, name), item);
+	}
+
+	//register items
+	public static void registerItems() {
+		Ori.LOGGER.debug("Registering Mod Items for" + Ori.MOD_ID);
+	}
 }
